db7c2afb4a2287a273243d01731ce6496f575a83
Donald-Edianm <kobbyedinamdonald@gmail.com>
2025-11-13 16:08:26 +0000

    Init: Configured project structure

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+require('express-async-errors');
+require('dotenv').config();
+
+const healthRoutes = require('./routes/health');
+const errorHandler = require('./middlewares/errorHandler');
+const notFound = require('./middlewares/notFound');
+
+const app = express();
+
+// Security middleware
+app.use(helmet());
+app.use(cors());
+
+// Logging
+app.use(morgan('combined'));
+
+// Body parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use('/health', healthRoutes);
+
+// Error handling middleware (must be last)
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = app;
---
+++ src/controllers/healthController.js
@@ -0,0 +1,7 @@
+const getHealth = (req, res) => {
+  res.json({ status: 'ok' });
+};
+
+module.exports = {
+  getHealth
+};
